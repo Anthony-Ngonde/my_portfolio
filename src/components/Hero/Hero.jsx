efc086ffb3109f9461e85e3ed85b26789f7a18ae
Anthony Ngonde <anto.ngonde@gmail.com>
2024-09-22 17:56:38 +0300

    Components completed

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './Hero.css'
 import profile_img from '../../assets/profile_img.svg'
+import AnchorLink from 'react-anchor-link-smooth-scroll'
 
 const Hero = () => {
   return (
@@ -9,7 +10,7 @@ const Hero = () => {
         <h1><span>I'm Anthony Ngonde,</span> fullstack developer based in Kenya</h1>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque ratione tempora deserunt non sit ad accusantium perferendis architecto nemo, numquam fuga perspiciatis sed animi cupiditate odio eaque reiciendis illum vel!</p>
         <div className='hero-action'>
-          <div className="hero-connect">Connect with me</div>
+          <div className="hero-connect"><AnchorLink className='anchor-link' offset={50} href='#contact'>Connect with me</AnchorLink></div>
           <div className='hero-cv'>My CV</div>
         </div>
     </div>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -20,7 +20,7 @@ const Navbar = () => {
             <li><AnchorLink className='anchor-link' offset={50} href='#work'><p onClick={()=>setmenu("work")}>Projects</p></AnchorLink>{menu==="work"?<img src={underline} alt=''/>:<></>}</li>
             <li><AnchorLink className='anchor-link' offset={50} href='#contact'><p onClick={()=>setmenu("contact")}>Contact</p></AnchorLink>{menu==="contact"?<img src={underline} alt=''/>:<></>}</li>
         </ul>
-        <div className="nav-connect">Connect With Me</div>
+        <div className="nav-connect"><AnchorLink className='anchor-link' offset={50} href='#contact'>Connect With Me</AnchorLink></div>
     </div>
   )
 }
